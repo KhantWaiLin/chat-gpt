e550c8d282ef12f08ef63fb03505d5badaf17f65
KhantWaiLin <khantkhant.cm@gmail.com>
2023-03-08 09:44:44 +0630

    update server link

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
 
     //fetch Data from server 
-    const response = await fetch("http://localhost:5000/", {
+    const response = await fetch("https://chat-gpt-r0oc.onrender.com", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
